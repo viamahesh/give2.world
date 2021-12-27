a980ba2521fd9281ccae6479b08b0f1d9a11f95e
Mahesh Rodrigo <viamahesh@gmail.com>
2021-12-27 13:13:15 -0600

    init

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,56 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Charity } = require('../models');
+const { signToken } = require('../utils/auth');
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+
+const resolvers = {
+  Query: {
+    charities: async (parent, { name }) => {
+      const params = {};
+
+      if (name) {
+        params.name = {
+          $regex: name
+        };
+      }
+      return await Charity.find(params);
+    },
+    charity: async (parent, { _id }) => {
+      return await Charity.findById(_id);
+    },
+    user: async (parent, args, context) => {
+      if (context.user) {
+        const user = await User.findById(context.user._id);
+        return user;
+      }
+
+      throw new AuthenticationError('Not logged in');
+    }
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addCharity: async (_, args) => {
+      const res = await Charity.create(args.charityData);
+      return res;
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+      const token = signToken(user);
+      return { token, user };
+    }
+  }
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,58 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Charity {
+    charityName: String
+    missionStatement: String
+    charityType: String
+    address1: String
+    address2: String
+    city: String
+    state: String
+    zipCode: String
+    contactPerson: String
+    email: String
+    phone: String
+    website: String
+  }
+
+  input CharityInput {
+    charityName: String!
+    missionStatement: String
+    charityType: String
+    address1: String
+    address2: String
+    city: String
+    state: String
+    zipCode: String
+    contactPerson: String
+    email: String
+    phone: String
+    website: String
+  }
+
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query {
+    charities(name: String): [Charity]
+    charity(_id: ID!): Charity
+    user: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    addCharity(charityData: CharityInput!): Charity
+    login(email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
