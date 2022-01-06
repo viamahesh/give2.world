7c8bf531057b799c8f7335be6dd9556b23c1ab16
Mahesh Rodrigo <viamahesh@gmail.com>
2022-01-06 11:40:38 -0600

    1

--- client/src/hooks/useDeleteCharityMutation.ts
+++ client/src/hooks/useDeleteCharityMutation.ts
@@ -1,15 +1,6 @@
 import { gql } from '@apollo/client';
 import { useMutation } from "@apollo/client";
 
-// export const DELETE_CHARITY = gql`
-//   mutation deleteCharity($id: ID!) {
-//     deleteCharity(_id: $id) {
-//       charityName
-//     }
-//   }
-// `;
-
-
 export const DELETE_CHARITY = gql`
 mutation deleteCharity($id: ID!) {
   deleteCharity(_id: $id) {
