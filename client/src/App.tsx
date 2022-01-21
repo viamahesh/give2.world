557dc8bf825385828817cff3aabb03c115ff7ec2
Mahesh Rodrigo <viamahesh@gmail.com>
2022-01-20 20:55:36 -0600

    new

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,10 +17,9 @@ import { Home } from './Components/Home';
 import { Login, SignUp } from './Components/User';
 import { UserProvider } from './providers';
 
-const PORT = process.env.PORT || 3001;
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: `http://${window.location.hostname}:${PORT}/graphql `,
+  uri: `/graphql `,
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
