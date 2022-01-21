c5dd8dca886e68c1bd36ab0ccaff7971e76dbdea
Mahesh Rodrigo <viamahesh@gmail.com>
2022-01-20 20:09:05 -0600

    df

--- client/src/Components/Request/Add/Add.tsx
+++ client/src/Components/Request/Add/Add.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useFormik } from 'formik';
+// @ts-ignore
 import MaskedInput from 'react-text-mask';
 import { toast } from 'react-toast';
 
